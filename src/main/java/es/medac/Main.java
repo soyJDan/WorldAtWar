81c972476cfc644d5b3e97e49795d884c1c00226
KarlosAMP85 <cpm0020@alu.medac.es>
2024-02-22 10:43:45 +0100

    main adaptado

--- src/main/java/es/medac/Main.java
+++ src/main/java/es/medac/Main.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-      //GestorFicheroEntrada lector = new GestorFicheroEntrada("C:\\Users\\carlo\\Desktop\\rutaArchivoXml\\generales.txt");
-   //lector.leerArchivo();
-   //lector.obtenerNombreGeneral();
+//      GestorFicheroEntrada lector = new GestorFicheroEntrada("C:\\Users\\carlo\\Desktop\\rutaArchivoXml\\generales.txt");
+//   lector.leerArchivo();
+//   lector.obtenerNombreGeneral();
+//   System.out.println(lector.getNombreDeGeneral());
     }
 }
